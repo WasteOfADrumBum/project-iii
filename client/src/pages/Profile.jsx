03a85f1428c6b1247592caa139af74b891d41e64
Max Gerringer <max.gerringer@gmail.com>
2020-11-10 12:42:33 -0500

    test address input component on page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
+import AddressForm from "../components/address/addressForm";
 
 const Profile = () => {
   return (
     <>
       <NavBar />
         <div className="container">Profile Page</div>
+      <AddressForm />
       <Footer />
     </>
   );
