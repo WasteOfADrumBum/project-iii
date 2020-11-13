fe87c5f06f536e4a8d982b61a48e78af297b1e08
Joshua Small <jmsmall89@gmail.com>
2020-11-12 20:38:13 -0500

    Contact Page Complete

--- client/src/components/address/addressInput.jsx
+++ client/src/components/address/addressInput.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import AddressItem from './addressItem';
-
+import React, { Component } from "react";
+import AddressItem from "./addressItem";
 
 class AddressInput extends Component {
   render() {
     return (
-        <AddressItem
-          label="Address"
-          value={this.props.query}
-          onChange={this.props.onChange}
-          placeholder="type an address" />
+      <AddressItem
+        label="Address"
+        value={this.props.query}
+        onChange={this.props.onChange}
+        placeholder="type an address"
+      />
     );
   }
 }
