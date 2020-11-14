3bddea228c6051ddf8a5e6a09e1e2c654e5f2a8f
Joshua Small <jmsmall89@gmail.com>
2020-11-14 11:59:43 -0500

    Address Fix

--- client/src/pages/LetsGo.jsx
+++ client/src/pages/LetsGo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
-import AddressForm from "../components/address/addressForm";
+import PlacesAutoComplete from "../components/places/places"
 import "../assets/styles/letsgo.scss";
 
 // TODO: Dropdown placeholder values to be replaced with saved locations from database
@@ -36,7 +36,7 @@ const LetsGo = () => {
                 </select>
               </div>
               <div className="col-md-12 pt-3">
-                <AddressForm />
+                <PlacesAutoComplete />
               </div>
             </div>
           </div>
@@ -57,7 +57,7 @@ const LetsGo = () => {
                 </select>
               </div>
               <div className="col-md-12 pt-3">
-                <AddressForm />
+              <PlacesAutoComplete />
               </div>
             </div>
           </div>
