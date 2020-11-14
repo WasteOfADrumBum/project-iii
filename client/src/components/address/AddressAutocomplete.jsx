fdf56f01f998fa74187583f44c5e42092d3290af
Max Gerringer <max.gerringer@gmail.com>
2020-11-14 12:33:09 -0500

    Merge branch 'main' of github.com:WasteOfADrumBum/project-iii into maxDev

--- client/src/components/address/AddressAutocomplete.jsx
+++ client/src/components/address/AddressAutocomplete.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
+  getZipCode
 } from "use-places-autocomplete";
 import useOnclickOutside from "react-cool-onclickoutside";
 
-const PlacesAutocomplete = () => {
+const AddressAutocomplete = () => {
   const {
     ready,
     value,
@@ -19,31 +20,37 @@ const PlacesAutocomplete = () => {
     debounce: 300,
   });
   const ref = useOnclickOutside(() => {
-    // When user clicks outside of the component, we can dismiss
-    // the searched suggestions by calling this method
     clearSuggestions();
   });
 
   const handleInput = (e) => {
-    // Update the keyword of the input element
     setValue(e.target.value);
   };
 
   const handleSelect = ({ description }) => () => {
-    // When user selects a place, we can replace the keyword without request data from API
-    // by setting the second parameter to "false"
+    
     setValue(description, false);
     clearSuggestions();
 
-    // Get latitude and longitude via utility functions
+    // Get latitude, longitude, and ZipCode
     getGeocode({ address: description })
       .then((results) => getLatLng(results[0]))
       .then(({ lat, lng }) => {
-        console.log("📍 Coordinates: ", { lat, lng });
+        console.log("Coordinates: ", { lat, lng });
       })
       .catch((error) => {
-        console.log("😱 Error: ", error);
+        console.log("Error: ", error);
       });
+
+    getGeocode({ address: description })
+      .then((results) => getZipCode(results[0], false))
+      .then((zipCode) => {
+        console.log("ZipCode: ", zipCode)
+    })
+      .catch((error) => {
+        console.log("Error: ", error)
+      });
+    
   };
 
   const renderSuggestions = () =>
@@ -66,11 +73,12 @@ const PlacesAutocomplete = () => {
         value={value}
         onChange={handleInput}
         disabled={!ready}
-        placeholder="Where are you going?"
+        placeholder="Begin typing an address"
       />
       {/* We can use the "status" to decide whether we should display the dropdown or not */}
       {status === "OK" && <ul>{renderSuggestions()}</ul>}
     </div>
   );
 };
-export default PlacesAutocomplete;
+
+export default AddressAutocomplete;
