170a8ccf82aef2958f76b997680aa869f55cd479
jhf1203 <jhf1203@gmail.com>
2020-11-14 20:22:48 -0500

    apiRoutes syntax

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = require("express").Router();
 const AppController = require("../controllers/AppController")(db);
 const AuthController = require("../controllers/AuthController");
+const userController = require("../controllers/userController")
 const User = require("../models/UserInfo");
 
 router.post("/login", AuthController.loginUser);
 router.post("/signup", AuthController.signup);
 
 // Protect all routes after this middleware
-router.use(authController.protect);
+router.use(AuthController.protect);
 
 // DELETE Routes
 router.delete("/deleteMe", userController.deleteMe);
@@ -27,11 +28,11 @@ router
   .delete(userController.deleteUser);
 
 // GET Routes
-router.get("/userInfo", appController.findUser);
-router.get("/vehicleInfo", appController.findVehicle);
+router.get("/userInfo", AppController.findUser);
+router.get("/vehicleInfo", AppController.findVehicle);
 
 // POST Routes
-router.post("/userInfo", appController.createUser);
+router.post("/userInfo", AppController.createUser);
 
 // Export router
 module.exports = router;
