e0ad8d6356953193b0cbc0d7fbac36c0ea2e1f95
Max Gerringer <max.gerringer@gmail.com>
2020-11-21 12:09:25 -0500

    map container component

---
+++ client/src/components/address/MapContainer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from "google-maps-react";
+import axios from "axios";
+const GOOGLE_MAPS_API_KEY = "secret key location"
+
+const MapContainer = (props) => {
+  let [state, setstate] = React.useState({
+    // stateful props
+  })
+  
+  const {places, latitude, longitude, google} = this.props;
+  return ( 
+    <Map google = {google}
+      initialCenter = {{
+        lat: Number(latitude),
+        lng: Number(longitude),
+      }}
+      onClick = {this.maplicked}
+      zoom={10}>
+
+      {
+        places.length ? places.map(place => {
+          return(
+            <Marker key={place.id}
+              name={place.location.name}
+              onClick={this.MarkerClick}
+              postition={{lat: place.location.latitude, lng: place.location.longitude}} />
+          )
+        })
+         : null
+      }
+
+    <InfoWindow
+      marker={this.state.activeMarker}
+      visible={this.state.showInfoWindow}>
+        <div>
+          <a href=""><h3>{this.state.selectedPlace.name}</h3></a>
+        </div>
+
+    </InfoWindow>
+
+    </Map>
+  )
+  
+}
+
+
+
+export default GoogleApiWrapper({apiKey: (GOOGLE_MAPS_API_KEY)})(MapContainer)
