64484a725fd677118ba661ff87b16fe32c98ca95
Mark <mark.speer11@gmail.com>
2020-11-21 14:26:30 -0500

    makes function and vehicle json

---
+++ client/src/utils/makes.js
@@ -0,0 +1,18 @@
+const vDB = require("./vehicle.json");
+
+
+const makeArr = [];
+  for (let i = 0; i < vDB.length; i++) {
+    // console.log(makeArr.indexOf(vDB[i].make));
+    if (makeArr.indexOf(vDB[i].make === -1)) {
+      makeArr.push(vDB[i].make)
+    }
+  };
+
+
+  const Makes = Array.from(new Set(makeArr));
+
+  console.log("Makes: ", Makes);
+
+  export default Makes;
+  
