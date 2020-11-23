70cfeb91e83f0694564d47be3dfd55236223188f
Joshua Small <jmsmall89@gmail.com>
2020-11-23 08:39:34 -0500

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -9,24 +9,6 @@ import { checkUser } from "../utils/UserVerify";
 const LandingPage = () => {
   React.useEffect(() => {
     // ! ↓↓↓ Moved to function in Client Utils ↓↓↓
-    // ! DO NOT REMOVE THIS CODE FROM HERE
-    // Check logged in User
-    // const checkUser = async () => {
-    //   try {
-    //     // Get Token
-    //     const token = localStorage.getItem("__token__");
-    //     console.log("LandingPage __token__", token);
-    //     // Throw Error if there is No Token
-    //     if (!token) throw new Error("No Token");
-    //     // Authorize token bearer
-    //     const response = await axios.get("/api/v1/users", {
-    //       headers: { Authorization: "Bearer " + token },
-    //     });
-    //     console.log("Authorized token bearer response:" + response);
-    //   } catch (error) {
-    //     console.log("checkUser catch " + error);
-    //   }
-    // };
     checkUser();
   }, []);
 
