e781402da11b6ea458367ea2255be2c0897e8dee
Max Gerringer <max.gerringer@gmail.com>
2020-11-24 20:50:23 -0500

    grab address props

--- client/src/pages/LetsGo.jsx
+++ client/src/pages/LetsGo.jsx
@@ -7,16 +7,50 @@ import "../assets/styles/letsgo.scss";
 import Map from "../components/address/MapContainer";
 import { withScriptjs } from "react-google-maps";
 import { CurrentUserContext } from "../utils/UserContext";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 // TODO: Dropdown placeholder values to be replaced with saved locations from database
 // TODO: onClick() Let's Go! btn send address information to map and map to accordion
 
 const LetsGo = () => {
-  const history = useHistory();
+  // const history = useHistory();
   const {firstName} = React.useContext(CurrentUserContext);
 
   const MapLoader = withScriptjs(Map);
+
+  let addressInfoTo;
+  const getAddressDataTo = (addressData) => {
+    if (
+      addressData.lat &&
+      addressData.lon &&
+      addressData.zip &&
+      addressData.street &&
+      addressData.city &&
+      addressData.state
+    ) {
+      addressInfoTo = addressData;
+    }
+  };
+
+  let addressInfoFrom;
+  const getAddressDataFrom = (addressData) => {
+    if (
+      addressData.lat &&
+      addressData.lon &&
+      addressData.zip &&
+      addressData.street &&
+      addressData.city &&
+      addressData.state
+    ) {
+      addressInfoFrom = addressData;
+    }
+  };
+
+  const handleButtonClick = async () => {
+    if(addressInfoFrom && addressInfoTo) {
+      console.log(addressInfoFrom, addressInfoTo)
+    }
+  };
 
   return (
     <>
@@ -47,7 +81,10 @@ const LetsGo = () => {
                   </select>
                 </div>
                 <div className="col-md-12 pt-3 address-content-container">
-                  <AddressAutocomplete />
+                  <AddressAutocomplete 
+                    getAddressData={getAddressDataFrom}
+                    shouldRunGetAddressDataCallback={true}
+                  />
                 </div>
               </div>
             </div>
@@ -68,14 +105,17 @@ const LetsGo = () => {
                   </select>
                 </div>
                 <div className="col-md-12 pt-3 address-content-container">
-                  <AddressAutocomplete />
+                  <AddressAutocomplete 
+                     getAddressData={getAddressDataTo}
+                     shouldRunGetAddressDataCallback={true}
+                  />
                 </div>
               </div>
             </div>
           </div>
           <div className="row text-center">
             <div className="col-md-12">
-              <button className="btn-success mt-2 mb-5 pt-2 pb-2 pr-4 pl-4">
+              <button className="btn-success mt-2 mb-5 pt-2 pb-2 pr-4 pl-4" onClick={handleButtonClick}>
                 Let's Go!
               </button>
             </div>
